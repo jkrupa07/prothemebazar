52272381ceb890f018eda8ef63526735cfe3aa70
isha <ishasondagar2@gmail.com>
2026-04-29 18:01:45 +0530

    faq page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2055,6 +2055,43 @@ var popper_lite_createPopper = /*#__PURE__*/popperGenerator({
 
 /***/ }),
 
+/***/ 212:
+/***/ (() => {
+
+// export class Accordion {
+//     init() {
+//         this.Accordion();
+//     }
+//     Accordion() {
+//         $(document).ready(function () {
+//             // Open the first child by default
+//             $('.closet-header').first().addClass('active').next('.closet-content').slideDown();
+
+//             // Handle click events for closet headers
+//             $('.closet-header').click(function () {
+//                 $(this).toggleClass('active').next('.closet-content').slideToggle();
+//                 $('.closet-header').not(this).removeClass('active').next('.closet-content').slideUp();
+//             });
+//         });
+//     }
+// }\
+
+$(document).ready(function () {
+  $(".faq-question").click(function () {
+    var answer = $(this).next(".faq-answer");
+
+    // Close others
+    $(".faq-answer").not(answer).slideUp();
+    $(".faq-question").not(this).removeClass("active");
+
+    // Toggle current
+    $(this).toggleClass("active");
+    answer.slideToggle();
+  });
+});
+
+/***/ }),
+
 /***/ 734:
 /***/ (function(module, __unused_webpack_exports, __webpack_require__) {
 
@@ -20482,39 +20519,8 @@ var Truncate = /*#__PURE__*/function () {
   }]);
   return Truncate;
 }();
-;// CONCATENATED MODULE: ./src/js/parts/accordion.js
-function accordion_typeof(obj) { "@babel/helpers - typeof"; return accordion_typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, accordion_typeof(obj); }
-function accordion_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function accordion_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, accordion_toPropertyKey(descriptor.key), descriptor); } }
-function accordion_createClass(Constructor, protoProps, staticProps) { if (protoProps) accordion_defineProperties(Constructor.prototype, protoProps); if (staticProps) accordion_defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-function accordion_toPropertyKey(arg) { var key = accordion_toPrimitive(arg, "string"); return accordion_typeof(key) === "symbol" ? key : String(key); }
-function accordion_toPrimitive(input, hint) { if (accordion_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (accordion_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
-var Accordion = /*#__PURE__*/function () {
-  function Accordion() {
-    accordion_classCallCheck(this, Accordion);
-  }
-  accordion_createClass(Accordion, [{
-    key: "init",
-    value: function init() {
-      this.Accordion();
-    }
-  }, {
-    key: "Accordion",
-    value: function Accordion() {
-      $(document).ready(function () {
-        // Open the first child by default
-        $('.closet-header').first().addClass('active').next('.closet-content').slideDown();
-
-        // Handle click events for closet headers
-        $('.closet-header').click(function () {
-          $(this).toggleClass('active').next('.closet-content').slideToggle();
-          $('.closet-header').not(this).removeClass('active').next('.closet-content').slideUp();
-        });
-      });
-    }
-  }]);
-  return Accordion;
-}();
+// EXTERNAL MODULE: ./src/js/parts/accordion.js
+var accordion = __webpack_require__(212);
 ;// CONCATENATED MODULE: ./src/js/parts/privacy.js
 function privacy_typeof(obj) { "@babel/helpers - typeof"; return privacy_typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, privacy_typeof(obj); }
 function privacy_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -20653,7 +20659,7 @@ jquery_default()(function () {
   window.parts.init();
   window.truncate = new Truncate();
   window.truncate.init();
-  window.accordion = new Accordion();
+  window.accordion = new accordion.Accordion();
   window.accordion.init();
   window.privacy = new Privacy();
   window.privacy.init();
--- src/js/parts/accordion.js
+++ src/js/parts/accordion.js
@@ -1,17 +1,38 @@
-export class Accordion {
-    init() {
-        this.Accordion();
-    }
-    Accordion() {
-        $(document).ready(function () {
-            // Open the first child by default
-            $('.closet-header').first().addClass('active').next('.closet-content').slideDown();
+// export class Accordion {
+//     init() {
+//         this.Accordion();
+//     }
+//     Accordion() {
+//         $(document).ready(function () {
+//             // Open the first child by default
+//             $('.closet-header').first().addClass('active').next('.closet-content').slideDown();
 
-            // Handle click events for closet headers
-            $('.closet-header').click(function () {
-                $(this).toggleClass('active').next('.closet-content').slideToggle();
-                $('.closet-header').not(this).removeClass('active').next('.closet-content').slideUp();
-            });
-        });
-    }
-}
+//             // Handle click events for closet headers
+//             $('.closet-header').click(function () {
+//                 $(this).toggleClass('active').next('.closet-content').slideToggle();
+//                 $('.closet-header').not(this).removeClass('active').next('.closet-content').slideUp();
+//             });
+//         });
+//     }
+// }\
+
+
+
+
+$(document).ready(function () {
+
+  $(".faq-question").click(function () {
+
+    let answer = $(this).next(".faq-answer");
+
+    // Close others
+    $(".faq-answer").not(answer).slideUp();
+    $(".faq-question").not(this).removeClass("active");
+
+    // Toggle current
+    $(this).toggleClass("active");
+    answer.slideToggle();
+
+  });
+
+});
